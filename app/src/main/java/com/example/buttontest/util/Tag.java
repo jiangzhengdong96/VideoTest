a953ba7a14d97c2738ab18962b205ef0bb01abfa
jiangzhengdong96 <jiangxiaoshe@foxmail.com>
2021-01-26 22:12:11 +0800

    播放视频

---
+++ app/src/main/java/com/example/buttontest/util/Tag.java
@@ -0,0 +1,14 @@
+package com.example.buttontest.util;
+
+/**
+ * 播放器标签
+ */
+public final class Tag {
+    //列表播放
+
+    //无缝播放
+    public static final String SEAMLESS = "seamless";
+    //画中画
+    public static final String PIP = "pip";
+    public static final String LIST = "list";
+}
